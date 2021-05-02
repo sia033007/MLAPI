56d22a5f8a13a8503fcb7e95b62b3b4de7544bc0
DESKTOP-8IO3ALF\HeisenBerg <siavashmehmandoost@gmail.com>
2021-05-02 13:23:51 +0430

    secondcommit

---
+++ Assets/Scripts/NetworkSample.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAPI;
+
+public class NetworkSample : MonoBehaviour
+{
+    private void OnGUI() {
+        if(GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
+        if(GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
+    }
+}
